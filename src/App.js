6782bd7584a0737d56d241340c566ff55fd02abc
muritador5050 <muritador5050@gmail.com>
2025-01-25 22:00:23 +0100

    set the app route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import './App.css';
+import Puffers from './pages/Puffers';
+import Men from './pages/Men';
+import Women from './pages/Women';
+import Sale from './pages/Sale';
+import Accessories from './pages/Accessories';
+import Giftcard from './pages/Giftcard';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const lenght = 45;
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Winter-collection</h1>
-        <h3>hello murder fucker</h3>
+    <div className='container'>
+      <header className='container__header'>
+        <nav className='nav'>
+          <ul className='nav__list'>
+            <li className='nav__list--element'>
+              <Link to='/'>
+                <Puffers /> Puffers
+              </Link>
+            </li>
+            <li className='nav__list--element'>
+              <Link to='/men'>
+                <Men />
+              </Link>
+            </li>
+            <li className='nav__list--element'>
+              <Link to='/women'>
+                <Women />
+              </Link>
+            </li>
+
+            <li className='nav__list--element'>
+              <Link to='/sales'>
+                <Sale />
+              </Link>
+            </li>
+
+            <li className='nav__list--element'>
+              <Link to='/accessories'>
+                <Accessories />
+              </Link>
+            </li>
+
+            <li className='nav__list--element'>
+              <Link to='/giftcard'>
+                <Giftcard />
+              </Link>
+            </li>
+
+            <li className='nav__list--element'>cart</li>
+          </ul>
+        </nav>
       </header>
+      <div>
+        <Routes>
+          <Route path='/' element={<Puffers />} />
+          <Route path='/men' element={<Men />} />
+          <Route path='/women' element={<Women />} />
+          <Route path='/sales' element={<Sale />} />
+          <Route path='/accessories' element={<Accessories />} />
+          <Route path='/giftcard' element={<Giftcard />} />
+        </Routes>
+      </div>
+
+      <footer className='container__footer'>footer</footer>
     </div>
   );
 }
